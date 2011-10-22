b25b2c9a65e19122bfc709195fbaa03362d517dc
Pawel Kolodziejski <aquadran@users.sourceforge.net>
2011-10-22 09:58:05 +0200

    load only for old kernel

--- oe/recipes/ti/libdce/ducati-init.sh
+++ oe/recipes/ti/libdce/ducati-init.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+kernel=`uname -r | cut -c 1-8`
+if [ "$kernel" != "2.6.35.3" ]; then
+	echo "not loading Ducati firmware"
+	exit 0
+fi
+
 /usr/bin/killall syslink_daemon.out
 /usr/bin/syslink_daemon.out /lib/firmware/omap4/Ipc_MPUSYS_reroute_core0.xem3 /lib/firmware/omap4/dce_app_m3.xem3
