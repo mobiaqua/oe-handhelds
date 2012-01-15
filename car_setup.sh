db872f73354404eba40531598a27462fb44a8b61
Paweł Kołodziejski <aquadran@users.sourceforge.net>
2012-01-15 08:32:58 +0100

    add check for proper run script

--- car_setup.sh
+++ car_setup.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+[ "x$0" = "x./car_setup.sh" ] && echo "Script must run via sourcing like '. car_setup.sh'"; exit 1
+
 source setup.sh car
--- tv_setup.sh
+++ tv_setup.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+[ "x$0" = "x./tv_setup.sh" ] && echo "Script must run via sourcing like '. tv_setup.sh'" && exit 1
+
 source setup.sh tv
